565b1cfa2b94bb9c79b5744733ace6e8dd0b2e28
krishnasankar <sankarkrishna689@gmail.com>
2024-03-04 17:48:56 +0530

    Login page

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
 def home(request):
    return render(request, 'shop/home.html')
+
+def register(request):
+   if request.POST == 'POST':
+      form = UserCreationForm()
+      if form.is_valid():
+         form.save()
+      messages.succes(request, 'Account created successfully!')
+   else:
+      form = UserCreationForm()
+   context = {
+      'form': form,
+   }
+   return render(request, 'register.html', context)
+def loginView(request):
+   return render(request, 'login.html')
